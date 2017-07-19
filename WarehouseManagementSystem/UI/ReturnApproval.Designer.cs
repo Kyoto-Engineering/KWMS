887869dd1814709243bcddfcb16c9ba1f142420e
Shimul <shimul39ju@gmail.com>
2017-07-19 17:15:04 +0600

    Return Approval created

---
+++ WarehouseManagementSystem/UI/ReturnApproval.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace KyotoSalesManagementSystem.UI
+{
+    partial class ReturnRequest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 207);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Shipment Info";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(110, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Return Cause";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Delivery Order No";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(188, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 63);
+            this.textBox1.MaxLength = 300;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 132);
+            this.textBox1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Request Return";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ReturnRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 339);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReturnRequest";
+            this.Text = "ReturnRequest";
+            this.Load += new System.EventHandler(this.ReturnRequest_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WarehouseManagementSystem/UI/ReturnApproval.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+using WarehouseManagementSystem.DbGateway;
+using WarehouseManagementSystem.LoginUI;
+using ZXing;
+using ZXing.Common;
+
+namespace KyotoSalesManagementSystem.UI
+{
+    public partial class ReturnRequest : Form
+    {
+        private SqlCommand cmd;
+        ConnectionString Cs=new ConnectionString();
+        private SqlConnection con;
+        private SqlDataReader rdr;
+        private string impOd;
+        private DataGridViewRow dr;
+        private int checkvalue,smId,available;
+        private int SupplierId;
+        private int Sio,OI,RRid;
+        public string ShID;
+        private string shipmentOrderNo,clientId,quotationId,brandCode;
+        public Nullable<Int64> brandid;
+        private Dictionary<int,string> orderList=new Dictionary<int, string>();
+        private Dictionary<int, int> productList = new Dictionary<int, int>();
+
+        public ReturnRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string[] splitterArray= comboBox1.Text.Split('-');
+            RRid = Convert.ToInt32(splitterArray[3]);
+            var x = from entry in orderList
+                where entry.Value == comboBox1.Text
+                select entry.Key;
+            OI=x.FirstOrDefault();
+            string qry="SELECT  ProductQuotation.Sl, OutProduct.OutQty FROM  OutTable INNER JOIN OutProduct ON OutTable.OutId = OutProduct.OutId INNER JOIN DeliveryProduct ON OutProduct.DeliveryProductId = DeliveryProduct.DeliveryProductId INNER JOIN ProductQuotation ON DeliveryProduct.PQId = ProductQuotation.PQId where OutTable.OutId="+OI;
+            con = new SqlConnection(Cs.DBConn);
+            cmd.CommandText = qry;
+            cmd.Connection = con;
+            con.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                int SI = rdr.GetInt32(0);
+                int Qty = rdr.GetInt32(1);
+                productList.Add(SI,Qty);
+            }
+            con.Close();
+        }
+
+
+
+
+        private void ReturnRequest_Load(object sender, EventArgs e)
+        {
+            ComboLoad();
+
+
+        }
+
+        private void ComboLoad()
+        {
+            con = new SqlConnection(Cs.DBConn);
+            string qry =
+                "SELECT OutTable.OutId, ('RR' +'-'+CONVERT(varchar(12), Delivery.SClientId)+'-'+CONVERT(varchar(12),ReturnRequest.SlOfClient)+'-'+CONVERT(varchar(12),ReturnRequest.RRid )) as ref FROM ReturnRequest INNER JOIN OutTable ON ReturnRequest.OutId = OutTable.OutId INNER JOIN Delivery ON OutTable.DeliveryId = Delivery.DeliveryId where ReturnRequest.RRid not in (SELECT  RRId from ReturnApproval)";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                int OutId = rdr.GetInt32(0);
+                string reff =rdr.GetString(1);
+                orderList.Add(OutId,reff);
+            }
+            con.Close();
+            foreach (KeyValuePair<int,string> refPair in orderList)
+            {
+                comboBox1.Items.Add(refPair.Value);
+            }
+        }
+
+        private void ClearselectedProduct()
+        {
+            impOd = null;
+            textBox1.Clear();
+        }
+
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+
+                    button1.Enabled = false;
+                    con = new SqlConnection(Cs.DBConn);
+                    string q1 =
+                    "INSERT INTO ReturnApproval(RRId, EntryDate, UserId) VALUES  (" + OI + ",@d1," + LoginForm.uId2 + ")";
+                SqlTransaction trnas;
+                trnas = con.BeginTransaction();
+                    cmd = new SqlCommand(q1, con);
+                    cmd.Parameters.AddWithValue("@d1", DateTime.UtcNow.ToLocalTime());
+                cmd.Transaction = trnas;
+                    con.Open();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    string query ="UPDATE MasterStocks1 SET MQuantity = MQuantity + @d3 WHERE (Sl = @d2)";
+                    cmd = new SqlCommand(query, con);
+                    foreach (KeyValuePair<int,int> prdct in productList)
+                    {
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("@d2", prdct.Key);
+                        cmd.Parameters.AddWithValue("@d3", prdct.Value);
+                        cmd.ExecuteNonQuery();
+                    }
+                    cmd.Transaction.Commit();
+                    MessageBox.Show("Delivery Order Done");
+                    ComboLoad();
+                    button1.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    
+                    MessageBox.Show(ex.Message,"Error But We Are Roll Backing");
+                    cmd.Transaction.Rollback();
+                }
+                    
+                con.Close();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Give Return Cause");
+                    
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Select Delivery Order");
+            }
+        }
+        }
+    }
