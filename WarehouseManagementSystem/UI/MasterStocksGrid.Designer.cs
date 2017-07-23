2be4c526c99bfe23897a9b8b2c435e4b981e384d
Ikramunnesa <ikraafi@gmail.com>
2017-07-23 10:40:00 +0600

    master stocks grid ui added

---
+++ WarehouseManagementSystem/UI/MasterStocksGrid.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WarehouseManagementSystem.UI
+{
+    partial class MasterStocksGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MStockId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 294);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MStockId,
+            this.Sl,
+            this.MQuantity});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(347, 246);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MStockId
+            // 
+            this.MStockId.HeaderText = "MStockId";
+            this.MStockId.Name = "MStockId";
+            this.MStockId.ReadOnly = true;
+            // 
+            // Sl
+            // 
+            this.Sl.HeaderText = "Sl";
+            this.Sl.Name = "Sl";
+            this.Sl.ReadOnly = true;
+            // 
+            // MQuantity
+            // 
+            this.MQuantity.HeaderText = "MQuantity";
+            this.MQuantity.Name = "MQuantity";
+            this.MQuantity.ReadOnly = true;
+            // 
+            // MasterStocksGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 328);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MasterStocksGrid";
+            this.Text = "MasterStocksGrid";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MasterStocksGrid_FormClosed);
+            this.Load += new System.EventHandler(this.MasterStocksGrid_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MStockId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MQuantity;
+    }
+}
