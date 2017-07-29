305b702213c724b0ff6f47586fc86aaa1ca3c91d
Ikramunnesa <ikraafi@gmail.com>
2017-07-29 17:11:19 +0600

    reports ui and product movement report added

---
+++ WarehouseManagementSystem/Reports/ReportsUI.cs
@@ -0,0 +1,67 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WarehouseManagementSystem.UI;
+
+namespace WarehouseManagementSystem.Reports
+{
+    public partial class ReportsUI : Form
+    {
+        public ReportsUI()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMainUI frm= new frmMainUI();
+            frm.Show();
+        }
+
+        private void ReportsUI_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Hide();
+            frmMainUI frm = new frmMainUI();
+            frm.Show();
+        }
+
+        private void ProMovementButton_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "ProductNRelatedDB";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            ProductMovement cr = new ProductMovement();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+    }
+}
