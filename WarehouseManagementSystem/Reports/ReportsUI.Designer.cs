45a12989fce65946f49c99214147f0d635e1f09e
Ikramunnesa <ikraafi@gmail.com>
2017-07-29 17:11:09 +0600

    reports ui and product movement report added

---
+++ WarehouseManagementSystem/Reports/ReportsUI.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WarehouseManagementSystem.Reports
+{
+    partial class ReportsUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ProMovementButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ProMovementButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(635, 369);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // ProMovementButton
+            // 
+            this.ProMovementButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProMovementButton.Location = new System.Drawing.Point(16, 19);
+            this.ProMovementButton.Name = "ProMovementButton";
+            this.ProMovementButton.Size = new System.Drawing.Size(122, 61);
+            this.ProMovementButton.TabIndex = 0;
+            this.ProMovementButton.Text = "Product Movement";
+            this.ProMovementButton.UseVisualStyleBackColor = true;
+            this.ProMovementButton.Click += new System.EventHandler(this.ProMovementButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(539, 15);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ReportsUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 445);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ReportsUI";
+            this.Text = "ReportsUI";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ReportsUI_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ProMovementButton;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
